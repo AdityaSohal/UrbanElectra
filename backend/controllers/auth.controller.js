c02714c022c78a6b0bf1d906edd572db04ffb8e0
Aditya Sohal <adityasohal732@gmail.com>
2026-02-28 22:36:07 +0530

    set up the backend

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async(req, res,next) => {
+    res.send("signup route is called");
+}
+
+export const login = (req, res) => {
+    res.send("login route is called");
+}
+
+export const logout = (req, res) => {
+    res.send("logout route is called");
+}
