ac89124efc79650e2a1942fcb55c0a6e915279cd
Aditya Sohal <adityasohal732@gmail.com>
2026-03-05 15:40:24 +0530

    made product routes and auth middleware and product controllers

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.routes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.get("/health", (req, res) => {
     res.send("Server is healthy");
