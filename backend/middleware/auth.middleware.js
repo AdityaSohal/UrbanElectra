dabcc6b7506a897ab4e4a57cd3d1fca5fa5028cc
Aditya Sohal <adityasohal732@gmail.com>
2026-03-05 15:52:11 +0530

    made tiny fixes

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -4,21 +4,16 @@ import User from "../models/user.model.js";
 export const protectRoute = async (req, res, next) => {
 	try {
 		const accessToken = req.cookies.accessToken;
-
 		if (!accessToken) {
 			return res.status(401).json({ message: "Unauthorized - No access token provided" });
 		}
-
 		try {
 			const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
 			const user = await User.findById(decoded.userId).select("-password");
-
 			if (!user) {
 				return res.status(401).json({ message: "User not found" });
 			}
-
 			req.user = user;
-
 			next();
 		} catch (error) {
 			if (error.name === "TokenExpiredError") {
